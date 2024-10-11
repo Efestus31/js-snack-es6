ffe9d87fc221fcb0251917c3813bf532ba6a36dd
Efestus31 <biagiohd@gmail.com>
2024-10-11 14:54:38 +0200

    array snack 2 + template html

---
+++ snack2/main.js
@@ -0,0 +1,28 @@
+// Creazione di un array di oggetti per le squadre di calcio
+const squadre = [
+    {
+        nome: 'Juventus',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'Inter',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'AC Milan',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'Napoli',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'Roma',
+        punti: 0,
+        falli: 0,
+    },
+];
