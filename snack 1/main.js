7a6d9d9f7dfafe3a902b4482c3091503dd1a62ac
Efestus31 <biagiohd@gmail.com>
2024-10-11 14:33:09 +0200

    add array object

---
+++ snack 1/main.js
@@ -0,0 +1,24 @@
+//inizializzo un array bidimensionale 
+const bikes = [
+    {
+        nome: 'Trek Emonda SLR 9',
+        peso: 6.8,
+    },
+    {
+        nome: 'Specialized S-Works Tarmac SL7',
+        peso: 7.2,
+    },
+    {
+        nome: 'Canyon Ultimate CF SLX',
+        peso: 7.1,
+    },
+    {
+        nome: 'Bianchi Oltre XR4',
+        peso: 7.4,
+    },
+    {
+        nome: 'Giant TCR Advanced SL',
+        peso: 7.3,
+    },
+]
+
